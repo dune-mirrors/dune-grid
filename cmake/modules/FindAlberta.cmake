48084bb7b2d2c380362b65a043b715a2ebb8ad9c
Simon Praetorius <simon.praetorius@tu-dresden.de>
2021-02-16 14:52:20 +0100

    Use pkg-config to find Alberta

--- cmake/modules/FindAlberta.cmake
+++ cmake/modules/FindAlberta.cmake
@@ -11,10 +11,7 @@ Imported targets
 
 This module defines the following :prop_tgt:`IMPORTED` target:
 
-``Alberta::Alberta``
-  The libraries, flags, and includes to use for Alberta, if found.
-
-``Alberta::Alberta_XD``
+``Alberta::AlbertaGrid_[n]d``
   Dimension dependent library
 
 Result Variables
@@ -30,11 +27,9 @@ Cache Variables
 
 The following variables may be set to influence this module's behavior:
 
-``Alberta_ROOT``
-  Base directory of Alberta installation
-
-``ALBERTA_EXTRA_LIBS``
-  Libraries needed to link Alberta, e.g. m
+``CMAKE_PREFIX_PATH``
+  Directory containing the alberta-grid_[n]d.pc pkg-config files. Can also
+  be controlled by setting ``PKG_CONFIG_PATH`` environment variable.
 
 #]=======================================================================]
 
@@ -43,86 +38,42 @@ include(FeatureSummary)
 set_package_properties("Alberta" PROPERTIES
   DESCRIPTION "An adaptive hierarchical finite element toolbox and grid manager")
 
-set(ALBERTA_EXTRA_LIBS "m" CACHE FILEPATH "Extra libraries needed by alberta for linking.")
+set(ALBERTA_WORLD_DIMS)
+set(ALBERTA_GRID_VERSION)
+set(ALBERTA_GRID_PREFIX)
 
-# look for header alberta/alberta.h
-find_path(ALBERTA_INCLUDE_DIR "alberta/alberta.h")
-
-# check version, Alberta 2 or 3
-set(ALBERTA_VERSION 2.0)
-set(DUNE_ALBERTA_VERSION 0x200)
-find_file(ALBERTA_HEADER "alberta/alberta.h"
-  HINTS ${ALBERTA_INCLUDE_DIR}
-  NO_DEFAULT_PATH)
-if(ALBERTA_HEADER)
-  cmake_push_check_state()
-  list(APPEND CMAKE_REQUIRED_DEFINITIONS -DDIM_OF_WORLD=3 -DDEL_INDEX=0)
-  list(APPEND CMAKE_REQUIRED_INCLUDES ${ALBERTA_INCLUDE_DIR})
-
-  include(CheckStructHasMember)
-  check_struct_has_member("struct el_info" wall_bound ${ALBERTA_HEADER} ALBERTA_IS_VERSION_3)
-  if(ALBERTA_IS_VERSION_3)
-    set(ALBERTA_VERSION 3.0)
-    set(DUNE_ALBERTA_VERSION 0x300)
-  endif()
-  cmake_pop_check_state()
-endif()
-unset(ALBERTA_HEADER CACHE)
-
-# check for ltdl
-find_library(ALBERTA_LTDL_LIB "ltdl")
-if(ALBERTA_LTDL_LIB)
-  list(APPEND ALBERTA_EXTRA_LIBS "ltdl")
-endif()
-
-# look for alberta utilitiy libraries
-find_library(ALBERTA_UTIL_LIB
-  NAMES alberta_util alberta_utilities)
-
-
-# check for which dimensions are supported Alberta installation
-cmake_push_check_state()
-list(APPEND CMAKE_REQUIRED_LIBRARIES "${ALBERTA_EXTRA_LIBS};${ALBERTA_UTIL_LIB}")
-
-foreach(dim RANGE 1 9)
-  find_library(ALBERTA_${dim}D_LIB alberta_${dim}d)
-  if(ALBERTA_${dim}D_LIB)
-    check_library_exists(${ALBERTA_${dim}D_LIB} mesh_traverse "" ALBERTA_${dim}D_LIB_FOUND)
-    if(ALBERTA_${dim}D_LIB_FOUND)
-      list(APPEND ALBERTA_WORLD_DIMS ${dim})
+# search for Alberta using pkg-config
+find_package(PkgConfig)
+if(PkgConfig_FOUND)
+  foreach(dim RANGE 1 9)
+    if(Alberta_FIND_VERSION)
+      set(ALBERTA_GRID_DIM_MODULE "alberta-grid_${dim}d>=${Alberta_FIND_VERSION}")
+    else()
+      set(ALBERTA_GRID_DIM_MODULE "alberta-grid_${dim}d")
     endif()
-  endif()
-  mark_as_advanced(ALBERTA_${dim}D_LIB)
-endforeach(dim)
 
-cmake_pop_check_state()
-message(STATUS "Found alberta libraries for dimensions ${ALBERTA_WORLD_DIMS}")
+    pkg_check_modules(Alberta${dim}d ${ALBERTA_GRID_DIM_MODULE} QUIET IMPORTED_TARGET GLOBAL)
+    if(Alberta${dim}d_FOUND)
+      list(APPEND ALBERTA_WORLD_DIMS ${dim})
+      set(ALBERTA_GRID_VERSION ${Alberta${dim}d_VERSION})
+      set(ALBERTA_GRID_PREFIX ${Alberta${dim}d_PREFIX})
+    endif()
+  endforeach(dim)
+endif()
 
-# behave like a CMake module is supposed to behave
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args("Alberta"
   REQUIRED_VARS
-    ALBERTA_INCLUDE_DIR ALBERTA_UTIL_LIB ALBERTA_WORLD_DIMS
+    ALBERTA_GRID_PREFIX PkgConfig_FOUND
   VERSION_VAR
-    ALBERTA_VERSION
+    ALBERTA_GRID_VERSION
+  FAIL_MESSAGE "Could NOT find Alberta (set PKG_CONFIG_PATH to include the location of the alberta-grid_[n]d.pc files)"
 )
 
-mark_as_advanced(ALBERTA_INCLUDE_DIR ALBERTA_UTIL_LIB ALBERTA_WORLD_DIMS)
-
-if(Alberta_FOUND AND NOT TARGET Alberta::Alberta)
-  add_library(Alberta::Alberta UNKNOWN IMPORTED)
-  set_target_properties(Alberta::Alberta PROPERTIES
-    IMPORTED_LOCATION ${ALBERTA_UTIL_LIB}
-    INTERFACE_INCLUDE_DIRECTORIES ${ALBERTA_INCLUDE_DIR}
-    INTERFACE_LINK_LIBRARIES "${ALBERTA_EXTRA_LIBS}")
-
-  foreach(dim RANGE 1 9)
-    if(ALBERTA_${dim}D_LIB AND NOT Alberta::Alberta_${dim}D)
-      add_library(Alberta::Alberta_${dim}D UNKNOWN IMPORTED)
-      set_target_properties(Alberta::Alberta_${dim}D PROPERTIES
-        IMPORTED_LOCATION ${ALBERTA_${dim}D_LIB}
-        INTERFACE_COMPILE_DEFINITIONS ALBERTA_DIM=${dim})
-      target_link_libraries(Alberta::Alberta_${dim}D INTERFACE Alberta::Alberta)
+if(Alberta_FOUND)
+  foreach(dim ${ALBERTA_WORLD_DIMS})
+    if(NOT Alberta::AlbertaGrid_${dim}d)
+      add_library(Alberta::AlbertaGrid_${dim}d ALIAS PkgConfig::Alberta${dim}d)
     endif()
   endforeach(dim)
 endif()
